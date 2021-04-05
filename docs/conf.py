8c43fd5c9234c1a58bd6dcd887ae942a85c46b3b
bicbioeng1 <59669165+bicbioeng1@users.noreply.github.com>
2021-04-05 13:00:09 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,11 @@
 # HTML Options
 html_logo = 'basin_logo.png'
+html_theme = 'asteroid_sphinx_theme'
 html_theme_options = {              # currently all defaults
-    'analytics_id': 'UA-XXXXXXX-1',
-    'analytics_anonymize_ip': False,
+    #'analytics_id': 'UA-XXXXXXX-1',
+    #'analytics_anonymize_ip': False,
     'logo_only': False,
-    'display_version': True,
+    #'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
